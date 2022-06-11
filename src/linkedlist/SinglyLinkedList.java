60ac4b73fe6a1d8214f3d03a0663a131e710458c
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-06-11 12:57:09 +0530

    added: singly linked list

---
+++ src/linkedlist/SinglyLinkedList.java
@@ -0,0 +1,38 @@
+package linkedlist;
+
+public class SinglyLinkedList {
+    private ListNode head;
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void display(){
+        ListNode current = head;
+        while (current != null){
+            System.out.print(current.data + "-> ");
+            current = current.next;
+        }
+        System.out.print("null");
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(20);
+        ListNode third = new ListNode(30);
+        ListNode fourth = new ListNode(40);
+
+        // now linking the lists
+        sll.head.next = second;
+        second.next = third;
+        third.next = fourth;
+
+        sll.display();
+    }
+}
