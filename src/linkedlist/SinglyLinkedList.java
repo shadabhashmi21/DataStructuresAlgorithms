68f9b75b2be923af62c83036402316a09d4e59da
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-07-04 11:11:59 +0530

    added: containsLoop()

--- src/linkedlist/SinglyLinkedList.java
+++ src/linkedlist/SinglyLinkedList.java
@@ -226,6 +226,34 @@ public class SinglyLinkedList {
         prev.next = current.next;
     }
 
+    public void createLoopInLinkedList(){
+        ListNode first = new ListNode(1);
+        ListNode second = new ListNode(2);
+        ListNode third = new ListNode(3);
+        ListNode fourth = new ListNode(4);
+
+        head = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = first;
+    }
+
+    public void containsLoop(){
+        ListNode fastPtr = head;
+        ListNode slowPtr = head;
+
+        while (fastPtr != null && fastPtr.next != null){
+            fastPtr = fastPtr.next.next;
+            slowPtr = slowPtr.next;
+            if(fastPtr == slowPtr){
+                System.out.println("Loop Detected");
+                return;
+            }
+        }
+        System.out.println("Loop not detected");
+    }
+
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
         sll.head = new ListNode(10);
@@ -258,6 +286,9 @@ public class SinglyLinkedList {
 
         sll.display();
         sll.count();
+
+        //sll.createLoopInLinkedList();
+        sll.containsLoop();
 
         /*sll.reverse();
         sll.display();
