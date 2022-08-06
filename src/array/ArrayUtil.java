e8d2392952379e016b6438be37ba41f40944457b
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-08-06 12:33:18 +0530

    feat: reverse()

--- src/array/ArrayUtil.java
+++ src/array/ArrayUtil.java
@@ -27,9 +27,23 @@ public class ArrayUtil {
         printArray(resultArray);
     }
 
+    public void reverse(int arr[]){
+        int start = 0, end = arr.length -1;
+        while (start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        System.out.println("Reversed Array");
+        printArray(arr);
+    }
+
     public static void main(String[] args) {
         int[] arr = {2,3,4,5,7,9};
         ArrayUtil arrayUtil = new ArrayUtil();
         arrayUtil.removeEven(arr);
+        arrayUtil.reverse(arr);
     }
 }
