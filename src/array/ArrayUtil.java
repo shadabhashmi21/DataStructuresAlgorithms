086acc8bae6e8c969b41ba5a6b75618433b418ab
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-08-06 11:52:36 +0530

    feat: removeEven()

--- src/array/ArrayUtil.java
+++ src/array/ArrayUtil.java
@@ -7,4 +7,29 @@ public class ArrayUtil {
         }
         System.out.println();
     }
+
+    public void removeEven(int[] arr){
+        int oddCount = 0;
+        for (int i=0; i<arr.length; i++){
+            if (arr[i]%2 != 0){
+                oddCount++;
+            }
+        }
+        int[] resultArray = new int[oddCount];
+        int index = 0;
+        for (int i=0; i<arr.length; i++){
+            if (arr[i]%2 != 0){
+                resultArray[index] = arr[i];
+                index++;
+            }
+        }
+        System.out.println("After after removing even numbers");
+        printArray(resultArray);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,5,7,9};
+        ArrayUtil arrayUtil = new ArrayUtil();
+        arrayUtil.removeEven(arr);
+    }
 }
