883a64aec522fa273a1c1975043ee257c2ef1185
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-08-19 12:18:32 +0530

    feat: nextGreaterElement()

--- src/stack/Problems.java
+++ src/stack/Problems.java
@@ -1,5 +1,6 @@
 package stack;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Problems {
@@ -15,10 +16,31 @@ public class Problems {
             ch[i] = stack.pop();
         }
 
-        System.out.println(ch);
+        System.out.println("Reversed String = " + Arrays.toString(ch));
+    }
+
+    public static void nextGreaterElement(int[] arr){
+        int[] result = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i= arr.length-1; i>=0; i--){
+            if (!stack.isEmpty()){
+                while (!stack.isEmpty() && stack.peek() <= arr[i]){
+                    stack.pop();
+                }
+            }
+            if (stack.isEmpty()){
+                result[i] = -1;
+            }else {
+                result[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+        System.out.println("Next Greater Element = " + Arrays.toString(result));
     }
 
     public static void main(String[] args) {
         reverseString("Mango");
+        nextGreaterElement(new int[]{4,7,3,4,8,1});
     }
 }
