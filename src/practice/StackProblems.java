eb720b70f47c2dc5cc31f0729807cc946c11fe08
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-08-24 23:09:54 +0530

    revised: StackProblems

--- src/practice/StackProblems.java
+++ src/practice/StackProblems.java
@@ -16,4 +16,49 @@ public class StackProblems {
 
         System.out.println(Arrays.toString(ch));
     }
+
+    public static void nextGreaterElement(int[] arr){
+        int[] res = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i=arr.length-1; i>=0; i--){
+            if (!stack.isEmpty()){
+                while (!stack.isEmpty() && stack.peek() <= arr[i])
+                    stack.pop();
+            }
+
+            if(stack.isEmpty()){
+                res[i] = -1;
+            }else {
+                res[i] = stack.peek();
+            }
+            stack.push(arr[i]);
+        }
+
+        System.out.println(Arrays.toString(res));
+    }
+
+    public static void  isValidParenthesis(String s){
+        Stack<Character> stack = new Stack<>();
+
+        for (char c: s.toCharArray()){
+            if (c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }else {
+                if (stack.isEmpty()){
+                    System.out.println("Invalid Parenthesis");
+                    return;
+                }else {
+                    char top = stack.peek();
+                    if (c == ')' && top == '(' || c == '}' && top == '{' || c == ']' && top == '[')
+                        stack.pop();
+                    else {
+                        System.out.println("Invalid Parenthesis");
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println(stack.isEmpty() ? "Valid Parenthesis" : "Invalid Parenthesis");
+    }
 }
