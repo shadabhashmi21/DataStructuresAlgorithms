c6cda7fef6aaa5a88d94a3a219414fa8e81f21ae
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-09-04 19:07:27 +0530

    feat: insert()

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -12,4 +12,41 @@ public class BinarySearchTree {
             this.data = data;
         }
     }
+
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    public TreeNode insert(TreeNode root, int value) {
+        if(root == null) {
+            root = new TreeNode(value);
+            return root;
+        }
+
+        if(value < root.data) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    public void inOrder(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(7);
+        bst.insert(1);
+
+        bst.inOrder(bst.root);
+    }
 }
