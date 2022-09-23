0c8a25f519b9d44670b5f86bdf9909ac12f6ba0e
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-09-23 10:07:32 +0530

    revised: ArrayUtil

--- src/practice/ArrayUtil.java
+++ src/practice/ArrayUtil.java
@@ -2,7 +2,7 @@ package practice;
 
 public class ArrayUtil {
     public void printArray(int[] arr){
-        for(int i : arr){
+        for (int i : arr) {
             System.out.print(i + " ");
         }
         System.out.println();
@@ -10,23 +10,25 @@ public class ArrayUtil {
 
     public void removeEven(int[] arr){
         int oddCount = 0;
-        for(int i : arr){
-            if(i%2 != 0)
+        for (int i : arr) {
+            if (i % 2 != 0)
                 oddCount++;
         }
 
+        int[] result = new int[oddCount];
         int index = 0;
-        int[] oddArr = new int[oddCount];
-        for(int i : arr){
-            if(i%2 != 0)
-                oddArr[index++] = i;
+        for (int i : arr) {
+            if (i % 2 != 0){
+                result[index++] = i;
+            }
         }
-        printArray(oddArr);
+
+        printArray(result);
     }
 
-    public void reverse(int[] arr){
+    public void reverse(int[] arr) {
         int start = 0, end = arr.length - 1;
-        while(start < end) {
+        while (start < end) {
             int temp = arr[start];
             arr[start++] = arr[end];
             arr[end--] = temp;
@@ -34,53 +36,47 @@ public class ArrayUtil {
         printArray(arr);
     }
 
-    public void smallestElement(int[] arr){
+    public void findSmallestElement(int[] arr) {
         int min = arr[0];
-        for (int i : arr){
-            if(min > i)
-                min = i;
+        for (int i: arr) {
+            if (min > arr[i])
+                min = arr[i];
         }
-        System.out.println("Smallest element: " + min);
+        System.out.println("Smallest Element = " + min);
     }
 
-    public void secondMax(int[] arr){
+    public void findSecondMax(int[] arr) {
         int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
-        for (int i : arr){
+        for (int i: arr) {
             if (max < i){
                 secondMax = max;
                 max = i;
-            } else if (secondMax < i && max != i) {
+            }else if (secondMax < i && i != max)
                 secondMax = i;
-            }
         }
-        System.out.println("Second largest element: " + secondMax);
+        System.out.println("Second Largest Element = " + secondMax);
     }
 
-    public void moveZeros(int[] arr){
+    public void moveZeros(int[] arr) {
         int j = 0;
         for (int i=0; i<arr.length; i++){
-            if (arr[i] != 0 && arr[j] == 0) {
-                int temp = arr[i];
+            if (arr[i] != 0 && arr[j] == 0){
+                int temp =  arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
-            if(arr[j] != 0)
+            if (arr[j] != 0)
                 j++;
         }
         printArray(arr);
     }
 
-    public void findMissingNumber(int[] arr){
+    public void findMissingNumber(int[] arr) {
         int n = arr.length + 1;
-        int sum = n * (n+1)/2;
-        for (int i : arr) {
+        int sum = n * (n+1) / 2;
+        for (int i : arr)
             sum = sum - i;
-        }
-        System.out.println("Missing number: " + sum);
-    }
 
-    public static void main(String[] args) {
-        ArrayUtil arrayUtil = new ArrayUtil();
-        arrayUtil.findMissingNumber(new int[] {1,2,3,5,6});
+        System.out.println("Missing number: " + sum);
     }
 }
