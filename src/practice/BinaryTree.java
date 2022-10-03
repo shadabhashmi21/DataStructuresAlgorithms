87ec9b4fa14154b372b724a5b70db8bc622aea8d
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-10-03 13:40:00 +0530

    revised: BinaryTree

--- src/practice/BinaryTree.java
+++ src/practice/BinaryTree.java
@@ -6,6 +6,7 @@ import java.util.Stack;
 
 public class BinaryTree {
     private TreeNode root;
+    
     private static class TreeNode{
         private TreeNode left;
         private TreeNode right;
@@ -30,47 +31,48 @@ public class BinaryTree {
         second.right = fifth;
     }
 
-    public void preOrderRecursive(TreeNode root) {
-        if (root == null) return;
+    public void preOrderRecursive(TreeNode root){
+        if (root == null)
+            return;
 
-        System.out.println(root.data + " ");
+        System.out.print(root.data + " ");
         preOrderRecursive(root.left);
         preOrderRecursive(root.right);
     }
 
-    public void preOrderIterative(TreeNode root) {
+    public void preOrderIterative(TreeNode root){
         if (root == null) return;
+
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
-
         while (!stack.isEmpty()) {
             TreeNode temp = stack.pop();
-            System.out.println(root.data + " ");
+            System.out.println(temp.data + " ");
             if (temp.right != null)
                 stack.push(temp.right);
-            if(temp.left != null)
+            if (temp.left != null)
                 stack.push(temp.left);
         }
     }
 
-    public void inOrderRecursive(TreeNode root) {
+    public void inOrderRecursive(TreeNode root){
         if (root == null) return;
 
         inOrderRecursive(root.left);
-        System.out.println(root.data + " ");
+        System.out.print(root.data + " ");
         inOrderRecursive(root.right);
     }
 
-    public void inOrderIterative(TreeNode root) {
+    public void inOrderIterative(TreeNode root){
         if (root == null) return;
 
         Stack<TreeNode> stack = new Stack<>();
         TreeNode temp = root;
-        while (!stack.isEmpty() || temp != null) {
-            if (temp != null) {
+        while (!stack.isEmpty() || temp != null){
+            if (temp != null){
                 stack.push(temp);
                 temp = temp.left;
-            }else {
+            }else{
                 temp = stack.pop();
                 System.out.println(temp.data + " ");
                 temp = temp.right;
@@ -78,7 +80,7 @@ public class BinaryTree {
         }
     }
 
-    public void postOrderRecursive(TreeNode root) {
+    public void postOrderRecursive(TreeNode root){
         if (root == null) return;
 
         postOrderRecursive(root.left);
@@ -86,39 +88,36 @@ public class BinaryTree {
         System.out.println(root.data + " ");
     }
 
-    public void postOrderIterative(TreeNode root) {
+    public void postOrderIterative(TreeNode root){
         TreeNode current = root;
         Stack<TreeNode> stack = new Stack<>();
-
-        while (current != null || !stack.isEmpty()) {
-            if (current != null) {
+        while (!stack.isEmpty() || current != null){
+            if (current != null){
                 stack.push(current);
                 current = current.left;
-            }else {
+            }else{
                 TreeNode temp = stack.peek().right;
-                if (temp == null) {
+                if (temp == null){
                     temp = stack.pop();
-                    System.out.println(temp.data + " ");
+                    System.out.print(temp.data + " ");
                     while (!stack.isEmpty() && temp == stack.peek().right){
                         temp = stack.pop();
-                        System.out.println(temp.data + " ");
+                        System.out.print(temp.data + " ");
                     }
-                }else{
+                }else
                     current = temp;
-                }
             }
         }
     }
 
-    public void levelOrder(TreeNode root) {
+    public void levelOrder(TreeNode root){
         if (root == null) return;
 
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-
         while (!queue.isEmpty()){
             TreeNode temp = queue.poll();
-            System.out.println(temp.data + " ");
+            System.out.print(temp.data + " ");
             if (temp.left != null)
                 queue.offer(temp.left);
             if (temp.right != null)
@@ -127,14 +126,15 @@ public class BinaryTree {
     }
 
     public int findMax(TreeNode root) {
-        if (root == null) return Integer.MIN_VALUE;
+        if (root == null)
+            return Integer.MIN_VALUE;
 
         int result = root.data;
         int left = findMax(root.left);
         int right = findMax(root.right);
         if (left > result)
             result = left;
-        if (right > result)
+        if (right < result)
             result = right;
         return result;
     }
