1585b7e679d2459a1860d1cb6157602126180463
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-10-03 18:25:41 +0530

    revised: BinarySearchTree

--- src/practice/BinarySearchTree.java
+++ src/practice/BinarySearchTree.java
@@ -3,14 +3,18 @@ package practice;
 public class BinarySearchTree {
     private TreeNode root;
 
-    private static class TreeNode {
+    private static class TreeNode{
+        private final int data;
         private TreeNode left;
         private TreeNode right;
-        private final int data;
 
         private TreeNode(int data) {
             this.data = data;
         }
+    }
+
+    public void insert(int data) {
+        root = insert(root, data);
     }
 
     public TreeNode insert(TreeNode root, int value) {
@@ -27,29 +31,26 @@ public class BinarySearchTree {
         return root;
     }
 
-    public void insert(int value){
-        root = insert(root, value);
-    }
-
     public void inOrder(TreeNode root){
-        if (root == null) return;
+        if (root == null)
+            return;
 
         inOrder(root.left);
-        System.out.println(root.data + " ");
+        System.out.print(root.data + " ");
         inOrder(root.right);
-    }
-
-    public TreeNode search(TreeNode root, int key){
-        if (root == null || root.data == key)
-            return root;
-
-        if(key < root.data)
-            return search(root.left, key);
-        else
-            return search(root.right, key);
     }
 
     public TreeNode search(int key){
         return search(root, key);
+    }
+
+    public TreeNode search(TreeNode root, int key){
+        if (root == null  || root.data == key)
+            return root;
+
+        if (key < root.data)
+            return search(root.left, key);
+        else
+            return search(root.right, key);
     }
 }
