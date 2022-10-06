8f576cce99b609654edc35505d9f6f13ec71ff5b
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-10-06 11:25:26 +0530

    feat: BubbleSort

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,28 @@
+package sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void sort(int[] arr){
+        int n = arr.length;
+        boolean isSwapped;
+
+        for (int i = 0; i < n-1; i++) {
+            isSwapped = false;
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSwapped = true;
+                }
+            }
+            if (!isSwapped) break;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        sort(new int[]{5,4,8,1,9});
+    }
+}
