a3cc511cc939976d84b77b4ce88301d14564c4b3
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-10-31 14:04:38 +0530

    revised: Sorted Matrix

--- src/practice/SortedMatrix.java
+++ src/practice/SortedMatrix.java
@@ -3,15 +3,15 @@ package practice;
 public class SortedMatrix {
     public static void search(int[][] arr, int n, int x){
         int i = 0, j = n-1;
-        while(i < n && j >= 0){
-            if(arr[i][j] == x){
+        while (i<n && j >= 0){
+            if (arr[i][j] == x){
                 System.out.println("Element found at: " + i + "," + j);
                 return;
             }
-            if(arr[i][j] > x)
-                j--;
-            else
+            if (arr[i][j] < x)
                 i++;
+            else
+                j--;
         }
         System.out.println("Element not found");
     }
