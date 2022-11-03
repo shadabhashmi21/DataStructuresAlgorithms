f34e8fa2cadcc5f6aa017c79592b0cf61b512239
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-03 11:46:43 +0530

    revised: Bubble Sort

---
+++ src/practice/BubbleSort.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class BubbleSort {
+    public static void sort(int[] arr){
+        int n = arr.length;
+        boolean isSwapped;
+
+        for (int i = 0; i < n-1; i++) {
+            isSwapped = false;
+            for (int j = 0; j < n-1-i; j++) {
+                if (arr[j] < arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSwapped = true;
+                }
+            }
+            if (!isSwapped) break;
+        }
+    }
+}
