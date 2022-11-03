da9605d48c3d7beadf0b344d358770a44f788a0f
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-03 12:41:25 +0530

    revised: Insertion Sort

---
+++ src/practice/InsertionSort.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class InsertionSort {
+    public static void sort(int[] arr){
+        for (int i=1; i<arr.length; i++){
+            int temp = arr[i];
+            int j = i-1;
+
+            while (j >= 0 && arr[j] > temp){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = temp;
+        }
+    }
+}
