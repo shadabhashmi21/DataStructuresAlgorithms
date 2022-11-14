c819eefa6f69714aea60fe3fc418507b7e61e644
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-14 15:44:02 +0530

    feat: AdjacentListGraph

---
+++ src/graph/AdjacentListGraph.java
@@ -0,0 +1,46 @@
+package graph;
+
+import java.util.LinkedList;
+
+public class AdjacentListGraph {
+    private LinkedList<Integer>[ ] adj;
+    private int V; // number of vertices
+    private int E; // number of edges
+
+    public AdjacentListGraph(int nodes) {
+        this.V = nodes;
+        this.E = 0;
+        this.adj = new LinkedList[nodes];
+        for(int v = 0; v < V; v++) {
+            adj[v] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adj[u].add(v);
+        adj[v].add(u);
+        E++;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(V + " vertices, " + E + " edges " + "\n");
+        for(int v = 0; v < V; v++) {
+            sb.append(v + ": ");
+            for(int w : adj[v]) {
+                sb.append(w + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        AdjacentListGraph g = new AdjacentListGraph(5);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+        System.out.println(g);
+    }
+}
